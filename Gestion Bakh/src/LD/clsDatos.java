ef16d7fe1733205d3ba9847025526d9ffd1ecfb9
alvarohusillos <43851862+alvarohusillos@users.noreply.github.com>
2019-03-21 22:04:41 +0100

    Add files via upload

--- Gestion Bakh/src/LD/clsDatos.java
+++ Gestion Bakh/src/LD/clsDatos.java
@@ -2,10 +2,8 @@ package LD;
 
 import java.sql.*;
 
-import COMUN.itfProperty;
-
-import static LD.clsConstantesDB.SQL_INSERT_ALUMNO;
-
+import static LD.clsConstantesDB.SQL_INSERT_SOCIO;
+import static LD.clsConstantesDB.SQL_INSERT_PROFESOR;
 
 
 public class clsDatos 
@@ -77,7 +75,8 @@ public class clsDatos
 		 *No tiene ningún retorno.
 		 */
 			
-		public void insertarSocio(itfProperty socios) 
+		public void insertarSocio(String nombre, String apellido1, String apellido2, String dni, String direccion,
+				String codigo_postal, String fecha_nacimiento, String telefono, String email, String iban) 
 		{
 				
 				// Instancias la clase que hemos creado anteriormente
@@ -89,14 +88,75 @@ public class clsDatos
 			    try { 
 				    if ( objConn != null ) {
 				    	// Preparamos la insert 
-				    	String query = "insert into socio (get, nombre, apellido, dni) values (?, ?, ?, ?)";
+				    	String query = SQL_INSERT_SOCIO;
 				    	
 				    	//Creamos las preparedstaments
 				    	PreparedStatement objSt = objConn.prepareStatement(query);
-				    	objSt.setInt(1, id);
-				    	objSt.setString(2, nombre);
-				    	objSt.setString(3, apellido);
+				    	
+				    	objSt.setString(1, nombre);
+				    	objSt.setString(2, apellido1);
+				    	objSt.setString(3, apellido2);
 				    	objSt.setString(4, dni);
+				    	objSt.setString(4, direccion);
+				    	objSt.setString(4, codigo_postal);
+				    	objSt.setString(4, fecha_nacimiento);
+				    	objSt.setString(4, telefono);
+				    	objSt.setString(4, email);
+				    	objSt.setString(4, iban);
+				    	
+				    	//Ejecutamos la query que hemos preparado
+				  		objSt.execute();
+				    	
+				    	System.out.println("Se ha insertado el registro correctamente");
+				    	
+				    	//Cerramos el preparedStatement
+				    	objSt.close();
+				    	
+				    	//Cerramos la conexión
+				    	objConn.close();
+				    	
+				    } else {
+				    	System.out.println("No existe conexión");
+				    }
+			    
+			    } catch (SQLException e) {
+			        System.out.println("No se ha podido realizar el insert: " + e);
+			    }
+			}
+		/**
+		 * @author Ignacio Chapero
+		 * 		   Alvaro Husillos
+		 * 
+		 *Este método se utiliza para insertar los datos de un nuevo profesor en la base de datos.Para ello 
+		 *primero comprueba que existe conexión con la BD y si la conexión es correcta, entonces inserta los
+		 *datos del nuevo profesor.
+		 *
+		 *No tiene ningún retorno.
+		 */
+			
+		public void insertarProfesor(String nombre, String apellido1, String apellido2, String dni, String direccion) 
+		{
+				
+				// Instancias la clase que hemos creado anteriormente
+			    clsDatos SQL = new clsDatos();
+			    
+				// Llamas al método que tiene la clase y te devuelve una conexión
+			    Connection objConn = SQL.conectarBD();
+			    
+			    try { 
+				    if ( objConn != null ) {
+				    	// Preparamos la insert 
+				    	String query = SQL_INSERT_PROFESOR;
+				    	
+				    	//Creamos las preparedstaments
+				    	PreparedStatement objSt = objConn.prepareStatement(query);
+				    	
+				    	objSt.setString(1, nombre);
+				    	objSt.setString(2, apellido1);
+				    	objSt.setString(3, apellido2);
+				    	objSt.setString(4, dni);
+				    	objSt.setString(4, direccion);
+				    	
 				    	
 				    	//Ejecutamos la query que hemos preparado
 				  		objSt.execute();
